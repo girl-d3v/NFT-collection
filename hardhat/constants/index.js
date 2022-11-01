928cdc1b0838c758eef598b4bec9556b743d98a0
girl-d3v <uponthisriot@gmail.com>
2022-10-31 22:26:00 -0600

    .

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x432752Ab3903442A29D08d9AECbf8189d8f7Ce28";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
